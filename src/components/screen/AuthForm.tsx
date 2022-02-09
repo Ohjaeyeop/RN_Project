ffdb1b95f572fd7541921b386065e16e9b7a5e57
OhJaeyeop <ohbcj0831@gmail.com>
2022-02-09 14:35:55 +0900

    회원가입 기능

--- src/components/screen/AuthForm.tsx
+++ src/components/screen/AuthForm.tsx
@@ -6,12 +6,45 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import {color} from '../../theme/color';
 
 const AuthForm = () => {
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    if (!username || !password) {
+      Alert.alert('입력되지 않은 필드가 있습니다.');
+      return;
+    }
+
+    const user = await firestore()
+      .collection('Users')
+      .where('username', '==', username)
+      .get();
+
+    if (user.size === 0) {
+      firestore()
+        .collection('Users')
+        .add({
+          username: username,
+          password: password,
+        })
+        .then(() => {
+          console.log('User added');
+        })
+        .catch(err => {
+          Alert.alert(err.message);
+        });
+    } else {
+      Alert.alert('이미 존재하는 ID 입니다.');
+    }
+
+    setUserName('');
+    setPassword('');
+  };
 
   return (
     <View style={{padding: 50, flex: 1, justifyContent: 'center'}}>
@@ -21,17 +54,19 @@ const AuthForm = () => {
         autoCapitalize={'none'}
         autoCorrect={false}
         onChangeText={setUserName}
+        value={username}
       />
       <Text style={styles.text}>Password:</Text>
       <TextInput
         style={styles.input}
         secureTextEntry={true}
         onChangeText={setPassword}
+        value={password}
       />
       <TouchableOpacity style={[styles.button, {marginTop: 20}]}>
         <Text style={styles.buttonText}>로그인</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
         <Text style={styles.buttonText}>회원가입</Text>
       </TouchableOpacity>
     </View>
