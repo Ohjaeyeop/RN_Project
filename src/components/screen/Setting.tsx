3a5ea3d98877e4dbaa99f8bb207128f8d8b0b219
OhJaeyeop <ohbcj0831@gmail.com>
2022-02-15 16:20:04 +0900

    로그아웃

--- src/components/screen/Setting.tsx
+++ src/components/screen/Setting.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import ScreenHeader from '../shared/ScreenHeader';
 import styled from 'styled-components/native';
-import {Theme} from '../../theme/color';
+import {color, Theme} from '../../theme/color';
+import {View, Text, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useUser} from '../../providers/UserProvider';
 
-const SettingContainer = styled.View`
+const SettingContainer = styled.SafeAreaView`
   flex: 1;
   background-color: ${({theme}: {theme: Theme}) => theme.background};
 `;
 
 const Setting = () => {
+  const {setUser} = useUser();
+
   return (
     <SettingContainer>
       <ScreenHeader title={'설정'} />
+      <View style={{flex: 1, paddingHorizontal: 28}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={{color: color.gray, fontWeight: '700', fontSize: 12}}>
+            로그아웃
+          </Text>
+          <TouchableOpacity
+            style={{
+              borderWidth: 1,
+              borderColor: color.gray,
+              borderStyle: 'dashed',
+              padding: 2,
+            }}
+            onPress={() => setUser(undefined)}>
+            <Icon name={'md-arrow-forward'} size={20} color={color.gray} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SettingContainer>
   );
 };
